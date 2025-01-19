bf9110a41b099c200994b2454c2a76ea852d43cc
Hossam <hossamabdo7359@gmail.com>
2025-01-19 20:53:21 +0200

    [feat] login component:integrated with api

--- src/app/features/auth/components/login/login.component.ts
+++ src/app/features/auth/components/login/login.component.ts
@@ -1,10 +1,42 @@
+import { AuthService } from './../../services/auth.service';
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  loginForm = this._FormBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  });
 
+  constructor(
+    private _FormBuilder: FormBuilder,
+    private _AuthService: AuthService,
+    private _ToastrService: ToastrService
+  ) {}
+
+  test() {
+    console.log(localStorage.getItem('token'));
+  }
+
+  sendLoginData() {
+    console.log(this.loginForm.value);
+    this._AuthService.onSubmited(this.loginForm.value).subscribe({
+      next: (res) => {
+        console.log(res);
+        this._ToastrService.success(res.message);
+        localStorage.setItem('token', res.data.accessToken);
+        localStorage.setItem('profile', JSON.stringify(res.data.profile));
+      },
+      error: (err) => {
+        console.log(err);
+        this._ToastrService.error('Login Error', err.error.message);
+      },
+    });
+  }
 }
--- src/app/features/auth/services/auth.service.ts
+++ src/app/features/auth/services/auth.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private _HttpClient: HttpClient) {}
 
-  constructor() { }
+  onSubmited(data: any): Observable<any> {
+    return this._HttpClient.post(
+      `https://upskilling-egypt.com:3005/api/auth/login`,
+      data
+    );
+  }
 }
