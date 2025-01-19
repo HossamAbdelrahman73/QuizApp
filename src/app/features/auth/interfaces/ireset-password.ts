fc9066da6431cf258327ef0caa362303e56156a4
assmabaziz <assma.baziz@yahoo.com>
2025-01-19 20:24:51 +0300

    interfaces and integration with api finished

---
+++ src/app/features/auth/interfaces/ireset-password.ts
@@ -0,0 +1,5 @@
+export interface IResetPassword {
+  otp: string;
+  email: string;
+  password: string;
+}
--- src/app/features/auth/services/auth.service.ts
+++ src/app/features/auth/services/auth.service.ts
@@ -1,4 +1,9 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { IResetPassword } from '../interfaces/ireset-password';
+import { IChangePassword } from '../interfaces/ichange-password';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +11,11 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor() { }
+  private _HttpClient = inject(HttpClient);
+  onResetPassword(data: FormGroup): Observable<IResetPassword> {
+    return this._HttpClient.post<IResetPassword>('api/auth/reset-password', data)
+  }
+  onChangePassword(data: FormGroup):Observable<IChangePassword>{
+    return this._HttpClient.post<IChangePassword>('api/auth/change-password', data)
+ }
 }
