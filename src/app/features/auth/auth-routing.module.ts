57d56f5068e0f20dfbc1c1ec216eceb3068bd2cb
Hossam <hossamabdo7359@gmail.com>
2025-01-19 14:16:12 +0200

    create structure of project

---
+++ src/app/features/auth/auth-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ForgetPasswordComponent } from './components/forget-password/forget-password.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { ChangePasswordComponent } from './components/change-password/change-password.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AuthComponent,
+    children: [
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+      { path: 'register', component: RegisterComponent },
+      { path: 'forgot-password', component: ForgetPasswordComponent },
+      { path: 'reset-password', component: ResetPasswordComponent },
+      { path: 'change-password', component: ChangePasswordComponent },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class AuthRoutingModule {}
