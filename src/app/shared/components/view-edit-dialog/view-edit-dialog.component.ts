eb0ef113ab86671928ac8f2aea9550dabcf8f128
Hossam <hossamabdo7359@gmail.com>
2025-01-20 23:44:45 +0200

    create structue of dashboard

---
+++ src/app/shared/components/view-edit-dialog/view-edit-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-edit-dialog',
+  templateUrl: './view-edit-dialog.component.html',
+  styleUrl: './view-edit-dialog.component.scss'
+})
+export class ViewEditDialogComponent {
+
+}
