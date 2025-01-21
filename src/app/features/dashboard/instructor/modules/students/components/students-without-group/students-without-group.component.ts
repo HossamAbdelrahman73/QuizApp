f04709290cd173c3e52b4b3a56c4c694c51d7c8a
Hossam <hossamabdo7359@gmail.com>
2025-01-21 22:19:53 +0200

    [feat] student-without-group component: create design of popup( Add To Group )and integrated with api

--- src/app/features/dashboard/instructor/modules/students/components/students-without-group/students-without-group.component.ts
+++ src/app/features/dashboard/instructor/modules/students/components/students-without-group/students-without-group.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { ISpecificStudent } from '../../interfaces/ispecific-student';
 import { Istudent } from '../../interfaces/istudent';
 import { StudentsService } from '../../services/students.service';
+import { IGroups } from '../../interfaces/igroups';
 declare var bootstrap: any; // Import Bootstrap JS globally
 
 @Component({
@@ -11,14 +12,22 @@ declare var bootstrap: any; // Import Bootstrap JS globally
   styleUrl: './students-without-group.component.scss',
 })
 export class StudentsWithoutGroupComponent {
-  collection: Istudent[] = [];
+  collection: ISpecificStudent[] = [];
   page: number = 1;
   studentview: ISpecificStudent | undefined = {} as ISpecificStudent;
+  groups: IGroups[] = [];
+  choosenGroup: string = '';
+  IdStudent: string = '';
 
   constructor(
     private _StudentsService: StudentsService,
     private _ToastrService: ToastrService
   ) {}
+
+  // chooseGroup(id: string) {
+  //   this.choosenGroup = id;
+  //   console.log(this.choosenGroup);
+  // }
 
   ngOnInit(): void {
     this.getAllStudentswithoutGroup();
@@ -55,11 +64,34 @@ export class StudentsWithoutGroupComponent {
     });
   }
 
-  AddStudenttoGroup(idStudent: string, idGroup: string) {
-    this._StudentsService.onAddStudentToGroup(idStudent, idGroup).subscribe({
+  AddStudentToGroup() {
+    if (this.choosenGroup && this.IdStudent) {
+      this._StudentsService
+        .onAddStudentToGroup(this.IdStudent, this.choosenGroup)
+        .subscribe({
+          next: (res) => {
+            console.log(res);
+            this._ToastrService.success(res.message);
+            this.getAllStudentswithoutGroup();
+          },
+          error: (err) => {
+            console.log(err);
+          },
+        });
+    }
+  }
+
+  openGroupsToAddStudent(idStudent: string) {
+    this.IdStudent = idStudent;
+    this._StudentsService.getAllGroups().subscribe({
       next: (res) => {
         console.log(res);
-        this._ToastrService.success(res.message);
+        this.groups = res;
+        const modalElement = document.getElementById('AddStudentModal');
+        if (modalElement) {
+          const modalInstance = new bootstrap.Modal(modalElement);
+          modalInstance.show();
+        }
       },
       error: (err) => {
         console.log(err);
