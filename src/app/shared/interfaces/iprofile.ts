eae5f087cdf8ca3bc201ea6ec9ed923f2e7e48f2
assmabaziz <assma.baziz@yahoo.com>
2025-01-22 15:42:30 +0300

    features addded

---
+++ src/app/shared/interfaces/iprofile.ts
@@ -0,0 +1,8 @@
+export interface IProfile {
+  email?: string;
+  first_name?: string;
+  last_name?: string;
+  role?: string;
+  status?: string;
+  _id?: string;
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,10 +6,13 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { DeleteDialogComponent } from './components/delete-dialog/delete-dialog.component';
 import { ViewEditDialogComponent } from './components/view-edit-dialog/view-edit-dialog.component';
+import { HomeComponent } from './components/home/home.component';
+import { RouterModule } from '@angular/router';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
-  declarations: [NavbarComponent, SidebarComponent, DeleteDialogComponent, ViewEditDialogComponent],
-  imports: [CommonModule, ReactiveFormsModule, HttpClientModule],
+  declarations: [NavbarComponent, SidebarComponent, DeleteDialogComponent, ViewEditDialogComponent, HomeComponent, NotFoundComponent],
+  imports: [CommonModule, ReactiveFormsModule, HttpClientModule, RouterModule],
   exports: [
     ReactiveFormsModule,
     HttpClientModule,
