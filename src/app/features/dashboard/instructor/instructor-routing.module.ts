21a583f5a93b752a2c986fbc55b1eb66a210dca1
Hossam <hossamabdo7359@gmail.com>
2025-01-23 14:32:29 +0200

    create structure of quizes module

--- src/app/features/dashboard/instructor/instructor-routing.module.ts
+++ src/app/features/dashboard/instructor/instructor-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InstructorComponent } from './instructor.component';
+import { ViewQuizComponent } from './modules/quizes/components/view-quiz/view-quiz.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: InstructorComponent },
   {
     path: 'students',
@@ -11,11 +13,30 @@ const routes: Routes = [
         (m) => m.StudentsModule
       ),
   },
-  { path: 'groups', loadChildren: () => import('../instructor/modules/groups/groups.module').then(m => m.GroupsModule) },
+  {
+    path: 'groups',
+    loadChildren: () =>
+      import('../instructor/modules/groups/groups.module').then(
+        (m) => m.GroupsModule
+      ),
+  },
+  {
+    path: 'quizes',
+    loadChildren: () =>
+      import('./modules/quizes/quizes.module').then((m) => m.QuizesModule),
+  },
+  {
+    path: 'bank',
+    loadChildren: () =>
+      import('./modules/quizes/modules/bank/bank.module').then(
+        (m) => m.BankModule
+      ),
+  },
+  { path: 'view-quiz', component: ViewQuizComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class InstructorRoutingModule { }
+export class InstructorRoutingModule {}
