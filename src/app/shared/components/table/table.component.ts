65cf4389f9ab435ff87218e446e17fd16e3b932f
bisho515 <bishogeorge689@gmail.com>
2025-01-26 22:46:06 +0200

    [feat] table component: add pagination to the shared table

--- src/app/shared/components/table/table.component.ts
+++ src/app/shared/components/table/table.component.ts
@@ -5,39 +5,30 @@ import { MatTableDataSource } from '@angular/material/table';
 import { ITableColumnConfig, ITablePipe } from '../../interfaces/table/table-column-config.interface';
 import { DatePipe } from '@angular/common';
 import { TruncatePipe } from '../../pipes/truncate.pipe';
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-];
+
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss'
 })
 export class TableComponent {
+  page: number = 1;
+  itemsPerPage: number = 5;
+  pageNumbers: number[] = [];
   @Input() tableColumnsConfig: ITableColumnConfig[] = [];
   @Input() set tableBody(data: any) {
     this.displayedColumns = this.tableColumnsConfig.map((column) => column.key);
     this.dataSource.data = data;
+    this.updatePagination();
   }
+  paginatedData: any[] = [];
   displayedColumns: string[] = [];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  dataSource = new MatTableDataSource(this.tableBody);
 
   constructor(private _liveAnnouncer: LiveAnnouncer, private datePipe: DatePipe, private truncatePipe: TruncatePipe) {
+    for(let i = 1; i <= 10; i++) {
+      this.pageNumbers.push(i);
+    }
   }
 
   @ViewChild(MatSort) sort!: MatSort;
@@ -62,6 +53,15 @@ export class TableComponent {
       default:
         return value;
     }
+  }
+  onPageChange(page: number) {
+    this.page = page;
+    this.updatePagination();
+  }
+  updatePagination() {
+    const start = (this.page - 1) * this.itemsPerPage;
+    const end = start + this.itemsPerPage;
+    this.paginatedData = this.dataSource.data.slice(start, end);
   }
 
 }
